d8d65fe2efa77720b6bf6f8f5d135a424b54c341
Xuho <ngotranxuanhoa09062004@gmail.com>
2025-04-13 17:39:34 +0700

    add loger service

---
+++ Infrastructure/LogProvider/Log.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.LogProvider
+{
+    public class Log
+    {
+        private readonly ILogger<Log> _logger;
+
+        public Log(ILogger<Log> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Info(string message) => _logger.LogInformation(message);
+        public void Warn(string message) => _logger.LogWarning(message);
+        public void Error(string message, Exception? ex = null) => _logger.LogError(ex, message);
+    }
+}
