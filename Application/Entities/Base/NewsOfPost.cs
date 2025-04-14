adacca6277bfad8de7ae338409a3263a37ed9794
Tuns <nguyennttuan8604@gmail.com>
2025-04-14 11:31:57 +0700

    Create applicationDb about Forum to test

--- Application/Entities/Base/NewsOfPost.cs
+++ Application/Entities/Base/NewsOfPost.cs
@@ -8,9 +8,11 @@ namespace Application.Entities.Base
 {
     public class NewsOfPost
     {
+        public int NewsOfPostId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
         public string Link { get; set; } = string.Empty;
         public string Country { get; set; } = string.Empty;
+        public Post Post { get; set; }
     }
 }
---
+++ Application/Interfaces/IServices/IPostService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.IServices
+{
+    public interface IPostService
+    {
+        Task<IReadOnlyList<Post>> GetAllPosts();
+    }
+}
