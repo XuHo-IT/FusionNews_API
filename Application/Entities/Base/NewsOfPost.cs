7255b56bd2452b54134eb6bd5a4c6b825b50a4c0
Tuns <nguyennttuan8604@gmail.com>
2025-04-14 10:21:46 +0700

    Create model: post, new of post, comment of post

---
+++ Application/Entities/Base/NewsOfPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Entities.Base
+{
+    public class NewsOfPost
+    {
+        public string Title { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
