7660ec2060e8e6c293943b02612e27c8bc373d9b
Tuns <nguyennttuan8604@gmail.com>
2025-04-14 10:21:46 +0700

    Create model: post, new of post, comment of post

---
+++ FusionNews_API/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FusionNews_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            return Ok("Post Controller");
+        }
+    }
+}
