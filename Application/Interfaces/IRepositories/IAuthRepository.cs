78fd6c19bd9e7ca8c580be89df7c8020bd1580cb
thanh <nguyenledangthanh.a41922@gmail.com>
2025-04-15 21:55:11 +0700

    Fix: add repository and service for login/register

---
+++ Application/Interfaces/IRepositories/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using Application.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.IRepositories
+{
+    public interface IAuthRepository
+    {
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task<bool> IsUsernameTakenAsync(string username);
+        Task AddUserAsync(User user);
+    }
+}
---
+++ Infrastructure/EntityFramework/Repositories/AuthRepository.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces.IRepositories;
+using Application.Entities.Base;
+using FusionNews_API.Data;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EntityFramework.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly UserDbContext _context;
+
+        public AuthRepository(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task<bool> IsUsernameTakenAsync(string username)
+        {
+            return await _context.Users.AnyAsync(u => u.Username == username);
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
+
+
