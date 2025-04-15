3c216c7af2f9452ebff45689c8f74eb1a25651b1
Tuns <nguyennttuan8604@gmail.com>
2025-04-15 11:26:58 +0700

    Modify GetAllPosts, CreatePost function

---
+++ Infrastructure/EntityFramework/Consts/StoredExecFunction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.EntityFramework.Consts
+{
+    public static class StoredExecFunction
+    {
+        public const string CreatePost = "SELECT * FROM usf_create_post(@JInput::jsonb)"; // usf -> user stored function
+        public const string GetAllPosts = "SELECT * FROM usf_get_all_post()";
+    }
+}
