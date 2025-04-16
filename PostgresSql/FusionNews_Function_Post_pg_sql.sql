c0a69d14e1fadf277a0db7866f6672c14c2d5b78
Tuns <nguyennttuan8604@gmail.com>
2025-04-16 14:45:38 +0700

    Modify crud post

--- PostgresSql/FusionNews_Function_Post_pg_sql.sql
+++ PostgresSql/FusionNews_Function_Post_pg_sql.sql
@@ -1,37 +1,60 @@
 -- Get All Post function --
-CREATE OR REPLACE FUNCTION public.usf_get_all_post(
-	)
-    RETURNS TABLE(postid integer, title character varying, content character varying, newsofpostid integer, createdon timestamp with time zone) 
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-    ROWS 1000
-
-AS $BODY$
+CREATE OR REPLACE FUNCTION usf_get_all_post()
+RETURNS TABLE(
+	postid int, 
+	title varchar(100), 
+	content text, 
+	newsofpostid int, 
+	createdon timestamp with time zone
+	)AS $$
 BEGIN
     RETURN QUERY
     SELECT 
-        post.post_id::int,
+        post.post_id,
         post.title::VARCHAR,
-        post.content::VARCHAR,
-        post.news_of_post_id::int,
-        post.created_on::TIMESTAMP with time zone
+        post.content,
+        post.news_of_post_id,
+        post.created_on
     FROM post;
 END;
-$BODY$;
+$$ LANGUAGE plpgsql;
+
+-- Find Post by Id
+CREATE OR REPLACE FUNCTION usf_find_post_by_id(json_input jsonb)
+RETURNS TABLE(
+	postid int, 
+	title varchar(100), 
+	content text, 
+	newsofpostid int, 
+	createdon timestamp with time zone
+)AS $$
+DECLARE
+    id INT;
+BEGIN
+    -- Extract the 'id' value from the JSON input
+    id := (json_input->>'id')::INT;
+ 
+    -- Query the 'post' table based on the extracted id
+    RETURN QUERY
+    SELECT 
+        post.post_id::INTEGER,
+        post.title::VARCHAR,
+        post.content::TEXT, -- Corrected to match TEXT type
+        post.news_of_post_id::INTEGER,
+        post.created_on::TIMESTAMP WITH TIME ZONE
+    FROM post
+    WHERE post.post_id = id;
+END;
+$$ LANGUAGE plpgsql;
 
 -- Create Post function
-CREATE OR REPLACE FUNCTION public.usf_create_post(
-	json_input jsonb)
-    RETURNS void
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-AS $BODY$
+CREATE OR REPLACE FUNCTION usf_create_post(json_input jsonb)
+RETURNS void
+AS $$
 DECLARE
-    title TEXT;
-    content TEXT;
-	news_of_post_id INTEGER;
+    title varchar(100);
+    content TEXT ;
+	newsof_post_id int;
 BEGIN
     -- Trích xuất các giá trị từ JSON
     title := json_input->>'Title';
@@ -48,4 +71,40 @@ BEGIN
     END IF;
 
 END;
-$BODY$;
+$$ LANGUAGE plpgsql;
+
+-- Update Post
+CREATE OR REPLACE FUNCTION usf_update_post(json_input jsonb)
+returns void
+AS $$
+DECLARE
+    post_id int,
+    title varchar(100);
+    content TEXT ;
+BEGIN
+    -- Trích xuất các giá trị từ JSON
+    post_id := (json_input->>'PostId')::INT;
+    title := json_input->>'Title';
+    content := json_input->>'Content';
+    UPDATE post
+        SET 
+            title = COALESCE(title, title), -- Chỉ cập nhật nếu không NULL
+            content = COALESCE(content, content),
+        WHERE post_id = post_id;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Delete Post
+CREATE OR REPLACE FUNCTION usf_delete_post(json_input jsonb)
+RETURNS void
+AS $$
+DECLARE
+    id INT;
+BEGIN
+    -- Extract the 'id' value from the JSON input
+    id := (json_input->>'id')::INT;
+ 
+    DELETE FROM post
+    WHERE post_id 
+END;
+$$ LANGUAGE plpgsql;
